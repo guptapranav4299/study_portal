1b95bba417f660288b058b9c98eceff58da25016
Pranav Gupta <guptapranav4299@gmail.com>
2022-07-16 11:54:05 +0530

    notes saved to db done

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,25 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .models import *
+from .forms import *
 # Create your views here.
 def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
+    if request.method == 'POST':
+        form =  NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes added from {request.user.username} successfully!!")
+    else:
+        form = NotesForm()
+
     notes = Notes.objects.filter(user=request.user)
     context = {
-        'notes':notes
+        'notes':notes,
+        'form':form,
     }
     return render(request,'dashboard/notes.html',context)
+
